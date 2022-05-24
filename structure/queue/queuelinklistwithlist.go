149e3554ed7918d5bc1f0eafa78a689ec1373bf5
HongJinHyeok <redmax45@naver.com>
2022-05-24 20:06:52 +0900

    💬 컨테이너리스트 큐 코드리뷰

--- structure/queue/queuelinklistwithlist.go
+++ structure/queue/queuelinklistwithlist.go
@@ -9,10 +9,12 @@ type LQueue struct {
 	queue *list.List
 }
 
+// 맨 뒤 요소에 큐를 추가
 func (lq *LQueue) Enqueue(value any) {
 	lq.queue.PushBack(value)
 }
 
+// 맨 앞 요소 삭제
 func (lq *LQueue) Dequeue() error {
 	if !lq.Empty() {
 		element := lq.queue.Front()
@@ -24,6 +26,7 @@ func (lq *LQueue) Dequeue() error {
 	return fmt.Errorf("dequeue is empty we got an error")
 }
 
+// 맨 앞 요소 반환
 func (lq *LQueue) Front() (any, error) {
 	if !lq.Empty() {
 		val := lq.queue.Front().Value
@@ -33,6 +36,7 @@ func (lq *LQueue) Front() (any, error) {
 	return "", fmt.Errorf("error queue is empty")
 }
 
+// 맨 뒤 요소 반환
 func (lq *LQueue) Back() (any, error) {
 	if !lq.Empty() {
 		val := lq.queue.Back().Value
@@ -42,10 +46,12 @@ func (lq *LQueue) Back() (any, error) {
 	return "", fmt.Errorf("error queue is empty")
 }
 
+// 큐의 길이 반환
 func (lq *LQueue) Len() int {
 	return lq.queue.Len()
 }
 
+// 큐가 비어있을 경우 true, 그렇지 않을 경우 false 반환
 func (lq *LQueue) Empty() bool {
 	return lq.queue.Len() == 0
 }
